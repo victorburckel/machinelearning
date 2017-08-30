89e19d892a72429205b93d040ffa37abcd2379aa
victorburckel <victor.burckel@gmail.com>
2017-08-30 23:57:40 +0200

    rework with pipeline

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -5,99 +5,52 @@ Created on Mon Aug 28 20:46:08 2017
 @author: Victor
 """
 
-from pandas import read_table
+import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn import model_selection
 from sklearn import linear_model
 from sklearn import metrics
+from sklearn import pipeline
 
-def read_data(filename, columns):
-    '''
-    Read the training / test data set
-    '''
-    
-    dataSet = read_table(
-            filename, 
-            encoding='latin-1',
-            sep=',',
-            skipinitialspace=True,
-            index_col=0,
-            usecols = ['PassengerId'] + columns,
-            header=0)
-    
-    #dataSet.dropna(inplace = True)
-    dataSet['Embarked'].fillna('Unknwon', inplace = True)
-    dataSet['Age'].fillna(30, inplace = True)
-    
-    return dataSet
-
-def encode_categorical_features(trainDataSet, testDataSet, categoricalColumns):
-    '''
-    Encode categorical features in numerical features
-    '''
-    
-    for categoricalColumn in categoricalColumns:
-        le = preprocessing.LabelEncoder()
-        trainDataSet[categoricalColumn] = le.fit_transform(trainDataSet[categoricalColumn])
-        testDataSet[categoricalColumn] = le.transform(testDataSet[categoricalColumn])
-
-def split_data_sets(trainDataSet, testDataSet, featuresColumns, predictionColumn):
-    '''
-    Convert training data set X, y
-    Convert test data set to X_test
-    Splits the test data set in training and validation set
-    '''
-    
-    X = np.array(trainDataSet[featuresColumns], dtype=np.float)
-    y = np.array(trainDataSet[predictionColumn], dtype=np.float)
-    X_test = np.array(testDataSet, dtype=np.float)
-    X_train, X_val, y_train, y_val = model_selection.train_test_split(X, y, test_size=0.3)
-    return X_train, X_val, y_train, y_val, X_test
-
-def normalize_features(X_train, X_val, X_test):
-    scaler = preprocessing.StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_val = scaler.transform(X_val)
-    X_test = scaler.transform(X_test)
-    
-    return X_train, X_val, X_test
-
-def train(X_train, X_val, y_train, y_val):
-    classifier = linear_model.LogisticRegression(C=1)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_val)
-    print(metrics.f1_score(y_val, y_pred))
-    print(metrics.accuracy_score(y_val, y_pred))
-    
-    y_test = classifier.predict(X_test)
-    return y_test
 
 if __name__ == '__main__':
-    trainingDataSetFile = 'train.csv'
-    testDataSetFile = 'test.csv'
-    featuresColumns = ['Pclass','Sex','Age','SibSp','Parch','Embarked']
-    predictionColumn = 'Survived'
-    categoricalColumns = ['Pclass','Sex','Embarked']
-    outputFile = 'prediction.csv'
+    # Read the training dataset
+    trainDataSet = pd.read_table('train.csv', encoding='utf-8', sep=',', index_col=0, header=0)
     
-    print('Reading training data set from %s' % trainingDataSetFile)
-    trainDataSet = read_data(trainingDataSetFile, featuresColumns + [predictionColumn])
+    # Fills in missing values with average value
+    trainDataSet['Embarked'].fillna('Unknwon', inplace = True)
+    trainDataSet['Age'].fillna(30, inplace = True)
     
-    print('Reading test data set from %s' % testDataSetFile)
-    testDataSet = read_data(testDataSetFile, featuresColumns)
+    # Generate dummy features from categorical features
+    X = pd.get_dummies(trainDataSet[['Age','SibSp','Parch','Pclass','Sex','Embarked']], columns = ['Pclass','Sex','Embarked'])
+    y = trainDataSet.Survived
     
-    print('Encoding categorical features')
-    encode_categorical_features(trainDataSet, testDataSet, categoricalColumns)
+    # Split the training set in training and test set
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
     
-    print('Splitting data sets')
-    X_train, X_val, y_train, y_val, X_test = split_data_sets(trainDataSet, testDataSet, featuresColumns, predictionColumn)
+    #Generate a pipeline that scales and applies logistic regression
+    pipe = pipeline.Pipeline(steps=[('scaler', preprocessing.StandardScaler()), ('logistic', linear_model.LogisticRegression())])
     
-    print('Normalizing features')
-    normalize_features(X_train, X_val, X_test)
+    # Find the best value for parameter C
+    estimator = model_selection.GridSearchCV(pipe, dict(logistic__C=np.logspace(-4, 4, 9)))
+    estimator.fit(X_train, y_train)
+    print(estimator.best_params_)
     
-    print('Training classifier')
-    y_test = train(X_train, X_val, y_train, y_val)
+    # Estimate the prediction on the test dataset
+    y_test_est = estimator.best_estimator_.predict(X_test)
+    print(metrics.f1_score(y_test, y_test_est))
+    print(metrics.accuracy_score(y_test, y_test_est))
+
+    # Read the dataset to predict
+    predictDataSet = pd.read_table('test.csv', encoding='utf-8', sep=',', index_col=0, header=0)
+    predictDataSet['Embarked'].fillna('Unknwon', inplace = True)
+    predictDataSet['Age'].fillna(30, inplace = True)
     
-    np.savetxt(outputFile, np.column_stack((testDataSet.index.values, y_test)), delimiter=',', header = 'PassengerId,Survived', fmt = '%i', comments='')
+    # Generate features and predict output
+    X_pred = pd.get_dummies(predictDataSet[['Age','SibSp','Parch','Pclass','Sex','Embarked']], columns = ['Pclass','Sex','Embarked'])
+    X_pred = X_pred.reindex(columns = X.columns, fill_value=0)
+    y_pred = estimator.best_estimator_.predict(X_pred)
     
+    # Save output to format for kaggle submission
+    np.savetxt('prediction.csv', np.column_stack((X_pred.index.values, y_pred)), delimiter=',', header = 'PassengerId,Survived', fmt = '%i', comments='')
